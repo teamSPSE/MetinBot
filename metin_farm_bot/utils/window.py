0f2ebe545a0029ae49965c1694783ece6169693f
jirka <jirka.vesely93@gmail.com>
2021-03-11 18:36:18 +0100

    bugfix

--- metin_farm_bot/utils/window.py
+++ metin_farm_bot/utils/window.py
@@ -182,7 +182,7 @@ class OskWindow(Window):
         self.press_key(button='Fn', mode='click')
         sleep(0.2)
         self.press_key(button=str(fkey), mode='click')
-        sleep(utils.get_relative_time(3))
+        sleep(utils.get_relative_time(5))
         self.press_key(button='Enter', mode='click')
         sleep(8)
 
